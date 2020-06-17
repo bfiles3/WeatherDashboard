b3fddeebd898e085faa5693021a2a5504da91ccc
bfiles3 <65058048+bfiles3@users.noreply.github.com>
2020-06-17 01:01:21 -0400

    Created a function to run on click of search key

--- Develop/script.js
+++ Develop/script.js
@@ -2,21 +2,32 @@
 var city = $("#cityInput").val();
 var search = $("#searchBtn");
 var searchHistory = $("#searchHistory");
+var apiKey = "&appid=341a5ba6d1e80abcab308215c512cc88"
 // var date = $datepicker({dateFormat:"mm/dd/yy"}).datepicker("setDate",new Date());
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=341a5ba6d1e80abcab308215c512cc88";
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
 
 // get the value of the city on click of the search button
-search.on("click", function() {
-    console.log(city);
-}); 
+$("#cityInput").on("click",(function(event) { 
+	
+	if (event.keyCode === 13) { 
+		event.preventDefault();
+		$("#searchBtn").click(); 
+	} 
+}));
 
-// get weather information
-$.ajax({
-    url: queryURL, 
+$("#searchBtn").on("click", function() {
+ $('#fiveDay').addClass(".block")
+  $("#cityInput").val();
+  $("#cityInput").val("");
+
+  $.ajax({
+    url: queryUrl,
     method: "GET"
-})
-.then (function(response){
+  })
+  .then(function (response){
+
     console.log(response);
-})
 
+    console.log(response.name); 
 
+  })}) 
